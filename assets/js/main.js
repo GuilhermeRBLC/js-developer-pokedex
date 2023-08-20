16f10b8eb0bbf6b0dd91dbd097b76f1f80c48995
Guilherme <guilhermerblc@gmail.com>
2023-08-20 14:19:23 -0300

    Adicionando realces e o carregamentos de novos pokemons pelo rolar da pagina.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const loaderIndicator = document.getElementById('loader')
 
 const maxRecords = 151
 const limit = 10
-let offset = 0;
+let offset = 0
+let enableLoadMore = true
+
+/**
+ * Exibe e oculta um indicador de carregamento para indicar oa usuário que algo está acontecendo
+ * caso a conexão esteja lenta.
+ * @param {boolean} status O estado atual do indicador verdadeiro para vísivel e falso para invisível.
+ */
+function toogleLoader(status) {
+    loaderIndicator.style.display = status ? 'block' : 'none'
+}
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -17,31 +27,54 @@ function convertPokemonToLi(pokemon) {
                 </ol>
 
                 <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                    alt="${pokemon.name}">
             </div>
         </li>
     `
 }
 
 function loadPokemonItens(offset, limit) {
+    toogleLoader(true)
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+
+        pokemons.forEach( (pokemon, index) => {
+            setTimeout(() => {
+                const e = document.createElement('div')
+                e.innerHTML = convertPokemonToLi(pokemon)
+                pokemonList.append(e.childNodes[1])
+            }, 100 * index);
+        })
+
+        /*const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml*/
+
+        toogleLoader(false)
+        enableLoadMore = true
     })
 }
 
 loadPokemonItens(offset, limit)
 
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+/**
+ * Verifica se o usuário rolou a página até o fim, se sim, carrega novos pokemons.
+ * Quando carregar o máximo de registros remove a escuta do evento de rolagem da pagina, para não
+ * tentar carregar mais registros.
+ */
+function loadMoreListner() {
+    if(window.scrollY + window.innerHeight > document.body.offsetHeight - 10 && enableLoadMore) {
+        enableLoadMore = false
+        offset += limit
+        const qtdRecordsWithNexPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+        if (qtdRecordsWithNexPage >= maxRecords) {
+            const newLimit = maxRecords - offset
+            loadPokemonItens(offset, newLimit)
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
+            window.removeEventListener('scroll', loadMoreListner)
+        } else {
+            loadPokemonItens(offset, limit)
+        }
     }
-})
+}
+
+window.addEventListener('scroll', loadMoreListner)
